2f834e0077118c22d26f8982b8de4743c7e255dc
yamiew511 <yamiew511@gmail.com>
2023-10-03 14:01:52 +0800

    5.0.0 Can now properly parse JSON objects and arrays.

---
+++ HttpContextCatcher.Extension/Converters/BsonValueJsonConverter.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace HttpContextCatcher.Extension.Converters
+{
+    public class BsonValueJsonConverter : JsonConverter<BsonValue>
+    {
+        public override BsonValue Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException("Read is not supported for this converter.");
+        }
+
+        public override void Write(Utf8JsonWriter writer, BsonValue value, JsonSerializerOptions options)
+        {
+            JsonSerializer.Serialize(writer, BsonTypeMapper.MapToDotNetValue(value), options);
+        }
+    }
+}
---
+++ HttpContextCatcher.Extension/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,17 @@
+﻿using HttpContextCatcher.Extension.Converters;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IMvcBuilder AddBsonSerializer(this IMvcBuilder builder)
+        {
+            builder.AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new BsonValueJsonConverter());
+            });
+
+            return builder;
+        }
+    }
+}
